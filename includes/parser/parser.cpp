5602019f7fd0c0749d2a5b83f51172812f85ea48
Jacob Bigham <jacob@jacobtbigham.com>
2021-08-05 15:22:03 -0700

    updated Parser with documentation and some refactoring for intelligibility

--- includes/parser/parser.cpp
+++ includes/parser/parser.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file parser.cpp
+ * @author Jacob Bigham (jacob@jacobtbigham.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-08-05
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include "parser.h"
 #include <string>
 #include <cstring>
@@ -24,6 +34,10 @@ void Parser::set_string(char s[])
     tokenizer.set_string(s);
 }
 
+//helpers for parse_tree()
+void map_from_command(STokenizer& tokenizer, Token& t, mmap_ss& parsed, string command);
+void map_from_select(STokenizer& tokenizer, Token& t, mmap_ss& parsed);
+
 mmap_ss Parser::parse_tree()
 {
     parsed.clear();
@@ -32,27 +46,32 @@ mmap_ss Parser::parse_tree()
     tokenizer >> t;
     if (t.value() == "make") // make table
     {
-        parsed.insert("command", "make");
-        for (int i = 0; i < 3; ++i) tokenizer >> t; // eat " table "
-        tokenizer >> t;
-        parsed.insert("table_name", t.value());
-        for (int i = 0; i < 3; ++i) tokenizer >> t; // eat " fields "
-        while (tokenizer.more())
-        {
-            tokenizer >> t;
-            if (t.type_string() == "ALPHA")
-            {
-                parsed.insert("col", t.value());
-            }
-        }
+        map_from_command(tokenizer, t, parsed, "make");
     }
     else if (t.value() == "create") // make table
     {
-        parsed.insert("command", "create");
-        for (int i = 0; i < 3; ++i) tokenizer >> t; // eat " table "
-        tokenizer >> t;
-        parsed.insert("table_name", t.value());
-        for (int i = 0; i < 3; ++i) tokenizer >> t; // eat " fields "
+        map_from_command(tokenizer, t, parsed, "create");
+    }
+    else if (t.value() == "insert") // insert into
+    {
+        map_from_command(tokenizer, t, parsed, "insert");
+    }
+    else // select
+    {
+        map_from_select(tokenizer, t, parsed);
+    }
+    return parsed;
+}
+
+void map_from_command(STokenizer& tokenizer, Token& t, mmap_ss& parsed, string command)
+{
+    parsed.insert("command", command);
+    for (int i = 0; i < 3; ++i) tokenizer >> t; // eat " table "
+    tokenizer >> t;
+    parsed.insert("table_name", t.value());
+    for (int i = 0; i < 3; ++i) tokenizer >> t; // eat " fields "
+    if (command == "make" || command == "create")
+    {
         while (tokenizer.more())
         {
             tokenizer >> t;
@@ -62,13 +81,8 @@ mmap_ss Parser::parse_tree()
             }
         }
     }
-    else if (t.value() == "insert") // insert into
+    else // insert
     {
-        parsed.insert("command", "insert");
-        for (int i = 0; i < 3; ++i) tokenizer >> t; // eat " into "
-        tokenizer >> t;
-        parsed.insert("table_name", t.value());
-        for (int i = 0; i < 3; ++i) tokenizer >> t; // eat " values "
         while (tokenizer.more())
         {
             tokenizer >> t;
@@ -78,37 +92,37 @@ mmap_ss Parser::parse_tree()
             }
         }
     }
-    else // select
+}
+
+void map_from_select(STokenizer& tokenizer, Token& t, mmap_ss& parsed)
+{
+    parsed.insert("command", "select");
+    tokenizer >> t; // eat space
+    tokenizer >> t;
+    while (t.value() != "from")
     {
-        parsed.insert("command", "select");
-        tokenizer >> t; // eat space
-        tokenizer >> t;
-        while (t.value() != "from")
+        if (t.type_string() == "ALPHA" || t.value() == "*")
         {
-            if (t.type_string() == "ALPHA" || t.value() == "*")
+            parsed.insert("fields", t.value());
+        }
+        tokenizer >> t;
+    }
+    tokenizer >> t; // eat space
+    tokenizer >> t;
+    parsed.insert("table_name", t.value());
+    tokenizer >> t;
+    tokenizer >> t;
+    if (tokenizer.more()) // where present
+    {
+        parsed.insert("where", "yes");
+        for (int i = 0; i < 2; ++i) tokenizer >> t; // eat where
+        while (tokenizer.more())
+        {
+            if (t.type_string() != "SPACE")
             {
-                parsed.insert("fields", t.value());
+                parsed.insert("condition", t.value());
             }
             tokenizer >> t;
         }
-        tokenizer >> t; // eat space
-        tokenizer >> t;
-        parsed.insert("table_name", t.value());
-        tokenizer >> t;
-        tokenizer >> t;
-        if (tokenizer.more()) // where present
-        {
-            parsed.insert("where", "yes");
-            for (int i = 0; i < 2; ++i) tokenizer >> t; // eat where
-            while (tokenizer.more())
-            {
-                if (t.type_string() != "SPACE")
-                {
-                    parsed.insert("condition", t.value());
-                }
-                tokenizer >> t;
-            }
-        }
     }
-    return parsed;
 }
--- includes/parser/parser.h
+++ includes/parser/parser.h
@@ -1,22 +1,69 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+/**
+ * @file parser.h
+ * @author Jacob Bigham (jacob@jacobtbigham.com)
+ * @brief Parses text into Tokens based on type (relational, literal, etc.)
+ * @version 0.1
+ * @date 2021-08-05
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include "../bplustree/multimap.h"
 #include "../table/typedefs.h"
 #include "../token/stokenize.h"
 #include "../token/token.h"
 
+/* The member STokenizer breaks an input into SQL-intelligible chunks:
+   The parse_tree() function creates and returns a multimap that maps textual
+   input from a SQL query to information that can be understood by the Table class.
+   For example, the query 'insert into students Jonathan Gomez 3.7 1460', would
+   pass to the Table class a multimap of the following form:
+   {
+       "command": ["insert"],
+       "table_name": ["students"],
+       "values": ["Jonathan", "Gomez", "3.7", "1460"]
+   }
+   The Table class processes this multimap accordingly.
+*/
+
 class Parser
 {
     public:
+        /**
+         * @brief Construct a new, empty Parser object
+         * 
+         */
         Parser();
+
+        /**
+         * @brief Construct a new Parser object with a given string to parse
+         * 
+         * @param s the SQL string to parse
+         */
         Parser(char s[]);
+
+        /**
+         * @brief Parse the input string and create a multimap relating command, table_name, and values
+         * 
+         * @return mmap_ss the multimap of command, table_name, and relevant values
+         */
         mmap_ss parse_tree();
+
+        /**
+         * @brief Set the input string
+         * 
+         * @param s the new input string to process
+         */
         void set_string(char s[]);
+
     private:
-        STokenizer tokenizer;
-        mmap_ss parsed;
-        char curr_input[300];
+        STokenizer tokenizer;   // tokenizes the input string
+        mmap_ss parsed;         // the multimap output from the last parse
+        char curr_input[300];    // the input string to parse
 };
 
 #endif
